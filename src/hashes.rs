624e7cf3d6497da9581226fdb1b753df67fd6427
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2017-01-15 22:17:54 +0100

    feat: move to ring

--- src/hashes.rs
+++ src/hashes.rs
@@ -10,21 +10,30 @@ pub enum HashTypes {
     /// SHA-512 (64-byte hash size)
     SHA2512,
     /// Encoding unsupported
-    SHA3,
+    SHA3512,
+    /// Encoding unsupported
+    SHA3384,
+    /// Encoding unsupported
+    SHA3256,
+    /// Encoding unsupported
+    SHA3224,
     /// Encoding unsupported
     Blake2b,
     /// Encoding unsupported
-    Blake2s
+    Blake2s,
 }
 
 impl HashTypes {
     /// Get the corresponding hash code
     pub fn code(&self) -> u8 {
         match *self {
-            HashTypes::SHA1    => 0x11,
+            HashTypes::SHA1 => 0x11,
             HashTypes::SHA2256 => 0x12,
             HashTypes::SHA2512 => 0x13,
-            HashTypes::SHA3    => 0x14,
+            HashTypes::SHA3512 => 0x14,
+            HashTypes::SHA3384 => 0x15,
+            HashTypes::SHA3256 => 0x16,
+            HashTypes::SHA3224 => 0x17,
             HashTypes::Blake2b => 0x40,
             HashTypes::Blake2s => 0x41,
         }
@@ -33,24 +42,30 @@ impl HashTypes {
     /// Get the hash length in bytes
     pub fn size(&self) -> u8 {
         match *self {
-	    HashTypes::SHA1     => 20,
-	    HashTypes::SHA2256  => 32,
-	    HashTypes::SHA2512  => 64,
-	    HashTypes::SHA3     => 64,
-	    HashTypes::Blake2b  => 64,
-	    HashTypes::Blake2s  => 32,
+            HashTypes::SHA1 => 20,
+            HashTypes::SHA2256 => 32,
+            HashTypes::SHA2512 => 64,
+            HashTypes::SHA3512 => 64,
+            HashTypes::SHA3384 => 64,
+            HashTypes::SHA3256 => 64,
+            HashTypes::SHA3224 => 64,
+            HashTypes::Blake2b => 64,
+            HashTypes::Blake2s => 32,
         }
     }
 
     /// Get the human readable name
     pub fn name(&self) -> &str {
         match *self {
-	    HashTypes::SHA1     => "SHA1",
-	    HashTypes::SHA2256  => "SHA2-256",
-	    HashTypes::SHA2512  => "SHA2-512",
-	    HashTypes::SHA3     => "SHA3",
-	    HashTypes::Blake2b  => "Blake-2b",
-	    HashTypes::Blake2s  => "Blake-2s",
+            HashTypes::SHA1 => "SHA1",
+            HashTypes::SHA2256 => "SHA2-256",
+            HashTypes::SHA2512 => "SHA2-512",
+            HashTypes::SHA3512 => "SHA3-512",
+            HashTypes::SHA3384 => "SHA3-384",
+            HashTypes::SHA3256 => "SHA3-256",
+            HashTypes::SHA3224 => "SHA3-224",
+            HashTypes::Blake2b => "Blake-2b",
+            HashTypes::Blake2s => "Blake-2s",
         }
     }
 
@@ -59,10 +74,13 @@ impl HashTypes {
             0x11 => Some(HashTypes::SHA1),
             0x12 => Some(HashTypes::SHA2256),
             0x13 => Some(HashTypes::SHA2512),
-            0x14 => Some(HashTypes::SHA3),
+            0x14 => Some(HashTypes::SHA3512),
+            0x15 => Some(HashTypes::SHA3384),
+            0x16 => Some(HashTypes::SHA3256),
+            0x17 => Some(HashTypes::SHA3224),
             0x40 => Some(HashTypes::Blake2b),
             0x41 => Some(HashTypes::Blake2s),
-            _    => None
+            _ => None,
         }
     }
 }
