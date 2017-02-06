a08be4585799ece994efe27ad8e605f14204a12a
maciejhirsz <maciej.hirsz@gmail.com>
2017-02-06 16:51:35 +0100

    Implemented Keccak and SHA3

--- src/hashes.rs
+++ src/hashes.rs
@@ -11,14 +11,22 @@ pub enum Hash {
     SHA2256,
     /// SHA-512 (64-byte hash size)
     SHA2512,
-    /// Encoding unsupported
+    /// SHA3-512 (64-byte hash size)
     SHA3512,
-    /// Encoding unsupported
+    /// SHA3-384 (48-byte hash size)
     SHA3384,
-    /// Encoding unsupported
+    /// SHA3-256 (32-byte hash size)
     SHA3256,
-    /// Encoding unsupported
+    /// SHA3-224 (28-byte hash size)
     SHA3224,
+    // Keccak-224 (28-byte hash size)
+    Keccak224,
+    // Keccak-256 (32-byte hash size)
+    Keccak256,
+    // Keccak-384 (48-byte hash size)
+    Keccak384,
+    // Keccak-512 (64-byte hash size)
+    Keccak512,
     /// Encoding unsupported
     Blake2b,
     /// Encoding unsupported
@@ -34,10 +42,14 @@ impl Hash {
             SHA1 => 0x11,
             SHA2256 => 0x12,
             SHA2512 => 0x13,
-            SHA3512 => 0x14,
-            SHA3384 => 0x15,
-            SHA3256 => 0x16,
             SHA3224 => 0x17,
+            SHA3256 => 0x16,
+            SHA3384 => 0x15,
+            SHA3512 => 0x14,
+            Keccak224 => 0x1A,
+            Keccak256 => 0x1B,
+            Keccak384 => 0x1C,
+            Keccak512 => 0x1D,
             Blake2b => 0x40,
             Blake2s => 0x41,
         }
@@ -49,8 +61,18 @@ impl Hash {
 
         match *self {
             SHA1 => 20,
-            SHA2256 | Blake2s => 32,
-            SHA2512 | SHA3512 | SHA3384 | SHA3256 | SHA3224 | Blake2b => 64,
+            SHA2256 => 32,
+            SHA2512 => 64,
+            SHA3224 => 28,
+            SHA3256 => 32,
+            SHA3384 => 48,
+            SHA3512 => 64,
+            Keccak224 => 28,
+            Keccak256 => 32,
+            Keccak384 => 48,
+            Keccak512 => 64,
+            Blake2b => 64,
+            Blake2s => 32,
 
         }
     }
@@ -67,6 +89,10 @@ impl Hash {
             SHA3384 => "SHA3-384",
             SHA3256 => "SHA3-256",
             SHA3224 => "SHA3-224",
+            Keccak224 => "Keccak-224",
+            Keccak256 => "Keccak-256",
+            Keccak384 => "Keccak-384",
+            Keccak512 => "Keccak-512",
             Blake2b => "Blake-2b",
             Blake2s => "Blake-2s",
         }
@@ -83,6 +109,10 @@ impl Hash {
             0x15 => SHA3384,
             0x16 => SHA3256,
             0x17 => SHA3224,
+            0x1A => Keccak224,
+            0x1B => Keccak256,
+            0x1C => Keccak384,
+            0x1D => Keccak512,
             0x40 => Blake2b,
             0x41 => Blake2s,
             _ => return Err(Error::UnkownCode),
