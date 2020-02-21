f73a232027038b664ef04fedfd06feeb83f7acac
Rüdiger Klaehn <rklaehn@gmail.com>
2020-02-21 13:39:23 +0100

    Add arbitraries
    Both for internal purposes and for use by other crates

---
+++ src/arb.rs
@@ -0,0 +1,32 @@
+use crate::hashes::{Hash, Hash::*};
+use crate::{encode, Multihash};
+use quickcheck::{Arbitrary, Gen};
+use rand::seq::SliceRandom;
+
+const HASHES: [Hash; 16] = [
+    Identity, SHA1, SHA2256, SHA2512, SHA3512, SHA3384, SHA3256, SHA3224, Keccak224, Keccak256,
+    Keccak384, Keccak512, Blake2b256, Blake2b512, Blake2s128, Blake2s256,
+];
+
+/// Generates a random hash algorithm.
+///
+/// The more exotic ones will be generated just as frequently as the common ones.
+impl Arbitrary for Hash {
+    fn arbitrary<G: Gen>(g: &mut G) -> Self {
+        *HASHES.choose(g).unwrap()
+    }
+}
+
+/// Generates a random valid multihash.
+///
+/// This is done by encoding a random piece of data.
+impl Arbitrary for Multihash {
+    fn arbitrary<G: Gen>(g: &mut G) -> Self {
+        let hash: Hash = Arbitrary::arbitrary(g);
+        let data: Vec<u8> = Arbitrary::arbitrary(g);
+        // encoding an actual random piece of data might be better than just choosing
+        // random numbers of the appropriate size, since some hash algos might produce
+        // a limited set of values
+        encode(hash, &data).unwrap()
+    }
+}
