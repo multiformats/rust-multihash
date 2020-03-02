f3f93983a370e9591c3620ee2593cbb373d5c0c9
Rüdiger Klaehn <rklaehn@gmail.com>
2020-03-02 13:49:36 +0100

    Use new API

--- src/arb.rs
+++ src/arb.rs
@@ -1,17 +1,16 @@
-use crate::hashes::{Hash, Hash::*};
-use crate::{encode, Multihash};
+use crate::{Code, Code::*, Multihash};
 use quickcheck::{Arbitrary, Gen};
 use rand::seq::SliceRandom;
 
-const HASHES: [Hash; 16] = [
-    Identity, SHA1, SHA2256, SHA2512, SHA3512, SHA3384, SHA3256, SHA3224, Keccak224, Keccak256,
-    Keccak384, Keccak512, Blake2b256, Blake2b512, Blake2s128, Blake2s256,
+const HASHES: [Code; 16] = [
+    Identity, Sha1, Sha2_256, Sha2_512, Sha3_512, Sha3_384, Sha3_256, Sha3_224, Keccak224,
+    Keccak256, Keccak384, Keccak512, Blake2b256, Blake2b512, Blake2s128, Blake2s256,
 ];
 
 /// Generates a random hash algorithm.
 ///
 /// The more exotic ones will be generated just as frequently as the common ones.
-impl Arbitrary for Hash {
+impl Arbitrary for Code {
     fn arbitrary<G: Gen>(g: &mut G) -> Self {
         *HASHES.choose(g).unwrap()
     }
@@ -22,11 +21,11 @@ impl Arbitrary for Hash {
 /// This is done by encoding a random piece of data.
 impl Arbitrary for Multihash {
     fn arbitrary<G: Gen>(g: &mut G) -> Self {
-        let hash: Hash = Arbitrary::arbitrary(g);
+        let code: Code = Arbitrary::arbitrary(g);
         let data: Vec<u8> = Arbitrary::arbitrary(g);
         // encoding an actual random piece of data might be better than just choosing
         // random numbers of the appropriate size, since some hash algos might produce
         // a limited set of values
-        encode(hash, &data).unwrap()
+        code.hasher().unwrap().digest(&data)
     }
 }
