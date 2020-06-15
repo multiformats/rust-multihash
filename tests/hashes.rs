e20c9c30bcf3b1a5e24f31429a623a1b6fda00ea
Volker Mische <volker.mische@gmail.com>
2020-06-15 15:13:03 +0200

    refactor: move non-test specific code out of test function
    With this refactor it is clearer what actually gets tested and
    additionally, things can be re-used in other tests.

--- tests/hashes.rs
+++ tests/hashes.rs
@@ -2,6 +2,65 @@ use std::convert::TryFrom;
 
 use multihash::{wrap, BoxedMultihashDigest, Code, MultihashDigest, MultihashGeneric, Sha3_512};
 
+enum MyCodeTable {
+    Foo = 1,
+    Bar = 2,
+}
+
+impl From<MyCodeTable> for u64 {
+    /// Return the code as integer value.
+    fn from(code: MyCodeTable) -> Self {
+        code as _
+    }
+}
+
+impl TryFrom<u64> for MyCodeTable {
+    type Error = String;
+
+    /// Return the `Code` based on the integer value. Error if no matching code exists.
+    fn try_from(raw: u64) -> Result<Self, Self::Error> {
+        match raw {
+            1 => Ok(MyCodeTable::Foo),
+            2 => Ok(MyCodeTable::Bar),
+            _ => Err("Cannot convert".to_string()),
+        }
+    }
+}
+
+#[derive(Default)]
+struct SameHash;
+impl SameHash {
+    pub const CODE: MyCodeTable = MyCodeTable::Foo;
+    /// Hash some input and return the sha1 digest.
+    pub fn digest(_data: &[u8]) -> MultihashGeneric<MyCodeTable> {
+        let digest = b"alwaysthesame";
+        wrap(Self::CODE, digest)
+    }
+}
+
+impl MultihashDigest<MyCodeTable> for SameHash {
+    #[inline]
+    fn code(&self) -> MyCodeTable {
+        Self::CODE
+    }
+    #[inline]
+    fn digest(&self, data: &[u8]) -> MultihashGeneric<MyCodeTable> {
+        Self::digest(data)
+    }
+    #[inline]
+    fn input(&mut self, _data: &[u8]) {}
+    #[inline]
+    fn result(self) -> MultihashGeneric<MyCodeTable> {
+        wrap(Self::CODE, b"alwaysthesame")
+    }
+    #[inline]
+    fn result_reset(&mut self) -> MultihashGeneric<MyCodeTable> {
+        wrap(Self::CODE, b"")
+    }
+    #[inline]
+    fn reset(&mut self) {}
+}
+
 #[test]
 fn to_u64() {
     assert_eq!(<u64>::from(Code::Keccak256), 0x1b);
@@ -21,65 +80,6 @@ fn hasher() {
 
 #[test]
 fn hasher_custom_codes() {
-    enum MyCodeTable {
-        Foo = 1,
-        Bar = 2,
-    }
-
-    impl From<MyCodeTable> for u64 {
-        /// Return the code as integer value.
-        fn from(code: MyCodeTable) -> Self {
-            code as _
-        }
-    }
-
-    impl TryFrom<u64> for MyCodeTable {
-        type Error = String;
-
-        /// Return the `Code` based on the integer value. Error if no matching code exists.
-        fn try_from(raw: u64) -> Result<Self, Self::Error> {
-            match raw {
-                1 => Ok(MyCodeTable::Foo),
-                2 => Ok(MyCodeTable::Bar),
-                _ => Err("Cannot convert".to_string()),
-            }
-        }
-    }
-
-    #[derive(Default)]
-    struct SameHash;
-    impl SameHash {
-        pub const CODE: MyCodeTable = MyCodeTable::Foo;
-        /// Hash some input and return the sha1 digest.
-        pub fn digest(_data: &[u8]) -> MultihashGeneric<MyCodeTable> {
-            let digest = b"alwaysthesame";
-            wrap(Self::CODE, digest)
-        }
-    }
-
-    impl MultihashDigest<MyCodeTable> for SameHash {
-        #[inline]
-        fn code(&self) -> MyCodeTable {
-            Self::CODE
-        }
-        #[inline]
-        fn digest(&self, data: &[u8]) -> MultihashGeneric<MyCodeTable> {
-            Self::digest(data)
-        }
-        #[inline]
-        fn input(&mut self, _data: &[u8]) {}
-        #[inline]
-        fn result(self) -> MultihashGeneric<MyCodeTable> {
-            wrap(Self::CODE, b"alwaysthesame")
-        }
-        #[inline]
-        fn result_reset(&mut self) -> MultihashGeneric<MyCodeTable> {
-            wrap(Self::CODE, b"")
-        }
-        #[inline]
-        fn reset(&mut self) {}
-    }
-
     impl From<MyCodeTable> for BoxedMultihashDigest<MyCodeTable> {
         fn from(code: MyCodeTable) -> Self {
             match code {
