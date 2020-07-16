f62a6e908941af6eac34d9c0bc9c55591163996d
mriise <mark.riise26@gmail.com>
2020-07-16 10:16:14 -0800

    use proper wording

--- tests/lib.rs
+++ tests/lib.rs
@@ -55,6 +55,7 @@ fn multihash_encode() {
         Blake2s256, b"hello world", "e0e402209aec6806794561107e594b1f6a8a6b0c92a0cba9acf5e5e93cca06f781813b0b";
         Blake2b256, b"hello world", "a0e40220256c83b297114d201b30179f3f0ef0cace9783622da5974326b436178aeef610";
         Blake2s128, b"hello world", "d0e4021037deae0226c30da2ab424a7b8ee14e83";
+        Blake3, b"hello world", "d74981efa70a0c880b8d8c1985d075dbcbf679b99a5f9914e5aaf96b831a9e24";
     }
 }
 
