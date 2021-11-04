8b46063a48c1e1a9d0a409875c64311375a968bb
Steven Allen <steven@stebalien.com>
2021-11-04 17:29:58 +0000

    remove per-hash digest type
    Remove the per-hasher digest type. Instead, store hash digests inside
    the hashers and "borrow" it. In all cases, we're going to copy it into a
    `Multihash<S>` anyways.
    
    This:
    
    1. Removes bunch of code.
    2. Means that hashers don't need to be generic over the size (unless
    they actually support multiple sizes). This fixes the UX issue
    introduced in the const generics PR.
    3. Avoids some copying.
    
    BREAKING CHANGE
    
    1. `Hasher.digest` no longer exists. Users should use
    `Code::SomeCode.digest` where possible.
    2. The hasher digests no longer exist.

--- src/multihash_impl.rs
+++ src/multihash_impl.rs
@@ -1,4 +1,4 @@
-use multihash_derive::Multihash;
+pub use multihash_derive::Multihash;
 
 /// Default (cryptographically secure) Multihash implementation.
 ///
@@ -11,69 +11,69 @@ use multihash_derive::Multihash;
 pub enum Code {
     /// SHA-256 (32-byte hash size)
     #[cfg(feature = "sha2")]
-    #[mh(code = 0x12, hasher = crate::Sha2_256, digest = crate::Sha2Digest<32>)]
+    #[mh(code = 0x12, hasher = crate::Sha2_256)]
     Sha2_256,
     /// SHA-512 (64-byte hash size)
     #[cfg(feature = "sha2")]
-    #[mh(code = 0x13, hasher = crate::Sha2_512, digest = crate::Sha2Digest<64>)]
+    #[mh(code = 0x13, hasher = crate::Sha2_512)]
     Sha2_512,
     /// SHA3-224 (28-byte hash size)
     #[cfg(feature = "sha3")]
-    #[mh(code = 0x17, hasher = crate::Sha3_224, digest = crate::Sha3Digest<28>)]
+    #[mh(code = 0x17, hasher = crate::Sha3_224)]
     Sha3_224,
     /// SHA3-256 (32-byte hash size)
     #[cfg(feature = "sha3")]
-    #[mh(code = 0x16, hasher = crate::Sha3_256, digest = crate::Sha3Digest<32>)]
+    #[mh(code = 0x16, hasher = crate::Sha3_256)]
     Sha3_256,
     /// SHA3-384 (48-byte hash size)
     #[cfg(feature = "sha3")]
-    #[mh(code = 0x15, hasher = crate::Sha3_384, digest = crate::Sha3Digest<48>)]
+    #[mh(code = 0x15, hasher = crate::Sha3_384)]
     Sha3_384,
     /// SHA3-512 (64-byte hash size)
     #[cfg(feature = "sha3")]
-    #[mh(code = 0x14, hasher = crate::Sha3_512, digest = crate::Sha3Digest<64>)]
+    #[mh(code = 0x14, hasher = crate::Sha3_512)]
     Sha3_512,
     /// Keccak-224 (28-byte hash size)
     #[cfg(feature = "sha3")]
-    #[mh(code = 0x1a, hasher = crate::Keccak224, digest = crate::KeccakDigest<28>)]
+    #[mh(code = 0x1a, hasher = crate::Keccak224)]
     Keccak224,
     /// Keccak-256 (32-byte hash size)
     #[cfg(feature = "sha3")]
-    #[mh(code = 0x1b, hasher = crate::Keccak256, digest = crate::KeccakDigest<32>)]
+    #[mh(code = 0x1b, hasher = crate::Keccak256)]
     Keccak256,
     /// Keccak-384 (48-byte hash size)
     #[cfg(feature = "sha3")]
-    #[mh(code = 0x1c, hasher = crate::Keccak384, digest = crate::KeccakDigest<48>)]
+    #[mh(code = 0x1c, hasher = crate::Keccak384)]
     Keccak384,
     /// Keccak-512 (64-byte hash size)
     #[cfg(feature = "sha3")]
-    #[mh(code = 0x1d, hasher = crate::Keccak512, digest = crate::KeccakDigest<64>)]
+    #[mh(code = 0x1d, hasher = crate::Keccak512)]
     Keccak512,
     /// BLAKE2b-256 (32-byte hash size)
     #[cfg(feature = "blake2b")]
-    #[mh(code = 0xb220, hasher = crate::Blake2b256, digest = crate::Blake2bDigest<32>)]
+    #[mh(code = 0xb220, hasher = crate::Blake2b256)]
     Blake2b256,
     /// BLAKE2b-512 (64-byte hash size)
     #[cfg(feature = "blake2b")]
-    #[mh(code = 0xb240, hasher = crate::Blake2b512, digest = crate::Blake2bDigest<64>)]
+    #[mh(code = 0xb240, hasher = crate::Blake2b512)]
     Blake2b512,
     /// BLAKE2s-128 (16-byte hash size)
     #[cfg(feature = "blake2s")]
-    #[mh(code = 0xb250, hasher = crate::Blake2s128, digest = crate::Blake2sDigest<16>)]
+    #[mh(code = 0xb250, hasher = crate::Blake2s128)]
     Blake2s128,
     /// BLAKE2s-256 (32-byte hash size)
     #[cfg(feature = "blake2s")]
-    #[mh(code = 0xb260, hasher = crate::Blake2s256, digest = crate::Blake2sDigest<32>)]
+    #[mh(code = 0xb260, hasher = crate::Blake2s256)]
     Blake2s256,
     /// BLAKE3-256 (32-byte hash size)
     #[cfg(feature = "blake3")]
-    #[mh(code = 0x1e, hasher = crate::Blake3_256, digest = crate::Blake3Digest<32>)]
+    #[mh(code = 0x1e, hasher = crate::Blake3_256)]
     Blake3_256,
 
     // The following hashes are not cryptographically secure hashes and are not enabled by default
     /// Identity hash (max. 64 bytes)
     #[cfg(feature = "identity")]
-    #[mh(code = 0x00, hasher = crate::IdentityHasher::<64>, digest = crate::IdentityDigest<64>)]
+    #[mh(code = 0x00, hasher = crate::IdentityHasher::<64>)]
     Identity,
 }
 
@@ -86,23 +86,27 @@ mod tests {
 
     #[test]
     fn test_hasher_256() {
-        let digest = Sha3_256::digest(b"hello world");
-        let hash = Code::multihash_from_digest(&digest);
+        let mut hasher = Sha3_256::default();
+        hasher.update(b"hello world");
+        let digest = hasher.finalize();
+        let hash = Code::Sha3_256.wrap(digest).unwrap();
         let hash2 = Code::Sha3_256.digest(b"hello world");
         assert_eq!(hash.code(), u64::from(Code::Sha3_256));
         assert_eq!(hash.size(), 32);
-        assert_eq!(hash.digest(), digest.as_ref());
+        assert_eq!(hash.digest(), digest);
         assert_eq!(hash, hash2);
     }
 
     #[test]
     fn test_hasher_512() {
-        let digest = Sha3_512::digest(b"hello world");
-        let hash = Code::multihash_from_digest(&digest);
+        let mut hasher = Sha3_512::default();
+        hasher.update(b"hello world");
+        let digest = hasher.finalize();
+        let hash = Code::Sha3_512.wrap(digest).unwrap();
         let hash2 = Code::Sha3_512.digest(b"hello world");
         assert_eq!(hash.code(), u64::from(Code::Sha3_512));
         assert_eq!(hash.size(), 64);
-        assert_eq!(hash.digest(), digest.as_ref());
+        assert_eq!(hash.digest(), digest);
         assert_eq!(hash, hash2);
     }
 }
