f3a4c8bbb2b6134d4101e0b5183bbf5af8489b37
Thomas Eizinger <thomas@eizinger.io>
2023-03-21 16:07:04 +0100

    Run rustfmt

--- src/multihash.rs
+++ src/multihash.rs
@@ -71,8 +71,14 @@ pub trait MultihashDigest<const S: usize>:
 /// assert_eq!(mh.size(), 32);
 /// assert_eq!(mh.digest(), &digest_bytes[2..]);
 /// ```
-#[cfg_attr(any(feature = "serde-codec", feature = "serde"), derive(serde::Deserialize))]
-#[cfg_attr(any(feature = "serde-codec", feature = "serde"), derive(serde::Serialize))]
+#[cfg_attr(
+    any(feature = "serde-codec", feature = "serde"),
+    derive(serde::Deserialize)
+)]
+#[cfg_attr(
+    any(feature = "serde-codec", feature = "serde"),
+    derive(serde::Serialize)
+)]
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialOrd)]
 pub struct Multihash<const S: usize> {
     /// The code of the Multihash.
@@ -80,7 +86,10 @@ pub struct Multihash<const S: usize> {
     /// The actual size of the digest in bytes (not the allocated size).
     size: u8,
     /// The digest.
-    #[cfg_attr(any(feature = "serde-codec", feature = "serde"), serde(with = "serde_big_array::BigArray"))]
+    #[cfg_attr(
+        any(feature = "serde-codec", feature = "serde"),
+        serde(with = "serde_big_array::BigArray")
+    )]
     digest: [u8; S],
 }
 
