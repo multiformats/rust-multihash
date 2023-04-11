110f98689b13627858ba14484497b728b87f42d4
Thomas Eizinger <thomas@eizinger.io>
2023-04-11 20:41:03 +0200

    Add identity example

--- examples/identity.rs
+++ examples/identity.rs
@@ -1,5 +1,18 @@
 use multihash::Multihash;
 
+//! An example for how to use the "identity" hash of `Multihash`.
+//!
+//! Identity hashing means we don't actually perform any hashing.
+//! Instead, we just store data directly in place of the "digest".
+//!
+//! [`Multihash::wrap`] returns an error in case the provided digest is too big for the available space.
+//! Make sure you construct a [`Multihash`] with a large enough buffer for your data.
+//!
+//! Typically, the way you want to use the "identity" hash is:
+//! 1. Check if your data is smaller than whatever buffer size you chose.
+//! 2. If yes, store the data inline.
+//! 3. If no, hash it make it fit into the provided buffer.
+
 fn main() {
     let valid_identity_hash = Multihash::<64>::wrap(0, b"foobar").unwrap();
     let invalid_identity_hash = Multihash::<2>::wrap(0, b"foobar");
