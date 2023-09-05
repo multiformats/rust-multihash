7ad51614ad347bfa8c6f421986abc517e04091f6
Volker Mische <volker.mische@gmail.com>
2023-09-05 20:06:08 +0200

    fix: make Serde (de)serialization no_std compatible (#337)
    `multihash` v0.19 introduced a regression. It's no longer possible to compile
    with `no_std` and `serde` feature enabled. This commit fixes that regression.
    
    Due to generic_const_exprs not being a thing yet, we need to use unsafe code
    to provide this functionality.

--- src/serde.rs
+++ src/serde.rs
@@ -1,20 +1,57 @@
 //! Multihash Serde (de)serialization
 
-use std::fmt;
+use core::{fmt, mem, slice};
 
 use serde::{
     de::{self, SeqAccess, Visitor},
-    Deserialize, Deserializer, Serialize, Serializer,
+    ser, Deserialize, Deserializer, Serialize, Serializer,
 };
 
 use crate::Multihash;
+
+/// The maximum serialization size of `code` is 9 bytes (a large varint encoded u64) and for `size`
+/// is 2 bytes  (a large varint encoded u8), this makes a total of 11 bytes.
+const MAXIMUM_PREFIX_SIZE: usize = 11;
+
+/// The is currently no way to allocate an array that is some constant size bigger then a given
+/// const generic. Once `generic_const_exprs` are a thing, this struct will no longer be needed.
+/// Until then we introduce a hack. We allocate a struct, which contains two independent arrays,
+/// which can be specified with const generics. We then treat the whole struct as a slice of
+/// continuous memory.
+#[repr(C, packed)]
+struct Buffer<const SIZE_FIRST: usize, const SIZE_SECOND: usize> {
+    first: [u8; SIZE_FIRST],
+    second: [u8; SIZE_SECOND],
+}
+
+#[allow(unsafe_code)]
+impl<const SIZE_FIRST: usize, const SIZE_SECOND: usize> Buffer<SIZE_FIRST, SIZE_SECOND> {
+    fn new() -> Self {
+        Self {
+            first: [0; SIZE_FIRST],
+            second: [0; SIZE_SECOND],
+        }
+    }
+
+    fn as_slice(&self) -> &[u8] {
+        unsafe { slice::from_raw_parts(self as *const _ as _, mem::size_of::<Self>()) }
+    }
+
+    fn as_mut_slice(&mut self) -> &mut [u8] {
+        unsafe { slice::from_raw_parts_mut(self as *mut _ as _, mem::size_of::<Self>()) }
+    }
+}
 
 impl<const SIZE: usize> Serialize for Multihash<SIZE> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        serializer.serialize_bytes(&self.to_bytes())
+        let mut buffer = Buffer::<MAXIMUM_PREFIX_SIZE, SIZE>::new();
+        let bytes_written = self
+            .write(buffer.as_mut_slice())
+            .map_err(|_| ser::Error::custom("Failed to serialize Multihash"))?;
+        serializer.serialize_bytes(&buffer.as_slice()[..bytes_written])
     }
 }
 
@@ -32,7 +69,7 @@ impl<'de, const SIZE: usize> Visitor<'de> for BytesVisitor<SIZE> {
         E: de::Error,
     {
         Multihash::<SIZE>::from_bytes(bytes)
-            .map_err(|err| de::Error::custom(format!("Failed to deserialize Multihash: {}", err)))
+            .map_err(|_| de::Error::custom("Failed to deserialize Multihash"))
     }
 
     // Some Serde data formats interpret a byte stream as a sequence of bytes (e.g. `serde_json`).
@@ -40,12 +77,21 @@ impl<'de, const SIZE: usize> Visitor<'de> for BytesVisitor<SIZE> {
     where
         A: SeqAccess<'de>,
     {
-        let mut bytes = Vec::new();
+        let mut buffer = Buffer::<MAXIMUM_PREFIX_SIZE, SIZE>::new();
+        let bytes = buffer.as_mut_slice();
+
+        // Fill the bytes slices with the given sequence
+        let mut pos = 0;
         while let Some(byte) = seq.next_element()? {
-            bytes.push(byte);
+            bytes[pos] = byte;
+            pos += 1;
+            if pos >= bytes.len() {
+                return Err(de::Error::custom("Failed to deserialize Multihash"));
+            }
         }
-        Multihash::<SIZE>::from_bytes(&bytes)
-            .map_err(|err| de::Error::custom(format!("Failed to deserialize Multihash: {}", err)))
+
+        Multihash::<SIZE>::from_bytes(&bytes[..pos])
+            .map_err(|_| de::Error::custom("Failed to deserialize Multihash"))
     }
 }
 
@@ -61,6 +107,8 @@ impl<'de, const SIZE: usize> Deserialize<'de> for Multihash<SIZE> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    use std::ptr;
 
     use serde_test::{assert_tokens, Token};
 
@@ -171,5 +219,32 @@ mod tests {
                 Token::SeqEnd,
             ],
         );
+    }
+
+    #[test]
+    fn test_buffer_alignment() {
+        const SIZE_FIRST: usize = 11;
+        const SIZE_SECOND: usize = 13;
+        let buffer = Buffer::<SIZE_FIRST, SIZE_SECOND>::new();
+
+        // Make sure that the struct allocated continuous memory, as we exploit that fact with the
+        // `as_slice` and `as_mut_slice()` methods.
+        let start_first = ptr::addr_of!(buffer.first) as *const u8;
+        let start_second = ptr::addr_of!(buffer.second) as *const u8;
+        #[allow(unsafe_code)]
+        unsafe {
+            assert_eq!(start_second.offset_from(start_first), SIZE_FIRST as isize);
+        };
+    }
+
+    #[test]
+    fn test_buffer() {
+        const SIZE_FIRST: usize = 3;
+        const SIZE_SECOND: usize = 8;
+        let mut buffer = Buffer::<SIZE_FIRST, SIZE_SECOND>::new();
+
+        let data: [u8; SIZE_FIRST + SIZE_SECOND] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        buffer.as_mut_slice().copy_from_slice(&data);
+        assert_eq!(buffer.as_slice(), data);
     }
 }
