e5d2db2ae661b15122047f25ab259b034d37c928
José Duarte <duarte.gmj@gmail.com>
2024-06-05 22:30:19 +0100

    fix: upgrade syn and synstructure (#362)

--- derive-impl/src/multihash.rs
+++ derive-impl/src/multihash.rs
@@ -97,7 +97,7 @@ impl<'a> From<&'a VariantInfo<'a>> for Hash {
         let mut code = None;
         let mut hasher = None;
         for attr in bi.ast().attrs {
-            let attr: Result<utils::Attrs<MhAttr>, _> = syn::parse2(attr.tokens.clone());
+            let attr: Result<utils::Attrs<MhAttr>, _> = syn::parse2(attr.meta.to_token_stream());
             if let Ok(attr) = attr {
                 for attr in attr.attrs {
                     match attr {
@@ -138,7 +138,9 @@ fn parse_code_enum_attrs(ast: &syn::DeriveInput) -> syn::LitInt {
     let mut alloc_size = None;
 
     for attr in &ast.attrs {
-        let derive_attrs: Result<utils::Attrs<DeriveAttr>, _> = syn::parse2(attr.tokens.clone());
+        let derive_attrs: Result<utils::Attrs<DeriveAttr>, _> =
+            syn::parse2(attr.meta.to_token_stream());
+
         if let Ok(derive_attrs) = derive_attrs {
             for derive_attr in derive_attrs.attrs {
                 match derive_attr {
@@ -192,10 +194,6 @@ fn error_code_duplicates(hashes: &[Hash]) {
         }
     });
 }
-
-/// An error that contains a span in order to produce nice error messages.
-#[derive(Debug)]
-struct ParseError(Span);
 
 pub fn multihash(s: Structure) -> TokenStream {
     let mh_crate = match utils::use_crate("multihash-derive") {
